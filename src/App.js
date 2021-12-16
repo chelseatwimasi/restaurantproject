746d049b719abf104ad035296612333279cab9f9
chelseatwimasi <89853022+chelseatwimasi@users.noreply.github.com>
2021-12-16 12:28:01 -0500

    project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cart: []
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React App</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
+//class App extends Component {
+//  constructor(props) {
+//    super(props);
+//this.state = {
+// cart: []
+// };
+//  }
+
+// render() {
+//   return (
+// <div className="App">
+// <h1>Chelsea and Dijah's Food Blog</h1>
+// </div>
+//   );
+// }
+//}
 
 export default App;
