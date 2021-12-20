e9454659fc25f62f2ecf502ecb4c59f6fbad9d03
chelseatwimasi <89853022+chelseatwimasi@users.noreply.github.com>
2021-12-20 12:27:45 -0500

    12/20/21

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+//import Card from "./Components/Card";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <h1>Chelly and Dijah's Food Blog</h1>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        Furbook
+        Recommended Top 10 Restaurants In NYC Based Off of Food
       </a>
       <button
         className="navbar-toggler"
